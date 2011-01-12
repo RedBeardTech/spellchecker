988e911b78c973651eed19f411dbe19fe3aa9b8f
Matt Hodgson <matthodgson@me.com>
2011-01-11 19:08:11 -0500

    updates for flags

--- lib/spellchecker.rb
+++ lib/spellchecker.rb
@@ -26,7 +26,7 @@ class Spellchecker
       results = spell_check_response.split("\n").slice(1..-1)
       result_index = 0
       response.each_with_index do |word_hash, index|
-        if word_hash[:original] =~ /[a-zA-z]/
+        if word_hash[:original] =~ /[a-zA-z\[\]\?]/
           if results[result_index] =~ ASPELL_WORD_DATA_REGEX
             response[index].merge!(:correct => false, :suggestions => results[result_index].split(':')[1].strip.split(',').map(&:strip))
           else
